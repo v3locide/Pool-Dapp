e4fe630d18cdb52e17529fb36c52a67058b2bfd8
v3locide <moh.abbassen99@gmail.com>
2024-11-16 10:29:22 +0000

    chore: forge init

--- provision.sh
+++ provision.sh
@@ -8,10 +8,15 @@ curl -L https://foundry.paradigm.xyz | bash
 source /home/vagrant/.bashrc
 foundryup
 
-# Setting up a new forge project
+# Installing Git
 apt install git -y
 git config --global user.email "moh.abbassen99@gmail.com"
 git config --global user.name "v3locide"
 
-
-#
+# Backend
+mkdir backend
+cd backend
+forge init --force
+rm -r script/* src/* test/*
+# Install Openzeppeling
+forge install OpenZeppelin/openzeppelin-contracts --no-commit
