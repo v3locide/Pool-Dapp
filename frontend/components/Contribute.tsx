8b7a5b7ad7c36ed9a7fb48becbd20ebc5afa64ec
mouloudamz <amzalimouloud@gmail.com>
2024-11-23 20:38:47 +0100

    add contibute

--- frontend/components/Contribute.tsx
+++ frontend/components/Contribute.tsx
@@ -1,45 +1,87 @@
 'use client'
 
 //chakraUi
-import { Flex,Text,Input,Button,Heading,useToast } from "@chakra-ui/react"
+import { Flex, Text, Input, Button, Heading, useToast } from "@chakra-ui/react"
+//Viem
+import { ParseAbiItem,Log } from "viem"
 //React 
 import { useState } from "react"
-//info de contrat
+//wagmi 
+import { useAccount,usePublicClient } from "wagmi"
+import { readContract,watchContractEvent } from "@wagmi/core" // get goal ,end ,total ,events  
+
+
 //Constants and Types
-import { contractAddress,abi } from "@/constants"
+import { contractAddress, abi } from "@/constants"
 import { parseEther } from "viem"
-import { prepareWriteContract,writeContract,waitForTransaction} from "@wagmi/core"
+//info de contrat
+import { prepareWriteContract, writeContract, waitForTransaction } from "@wagmi/core"
 
 const Contribute = () => {
-const [amount,setAmount ] = useState<string>('')
-const contribute = () => {
- 
 
-}
+    const toast = useToast()
+    const [amount, setAmount] = useState<string>('')
+    const contribute = async() => {
+        
+        try {
+            //convert ehter on wei
+            let money = parseEther(amount)
+            //preparation to write contract
+            const {request} = await 
+            prepareWriteContract({
+                address: contractAddress,
+                abi: abi,
+                functionName:"contribute",
+                value:money
+            })
+            // get transaction hash 
+            const {hash} = await writeContract (request)
+            const data = await waitForTransaction({
+                hash: hash
+            })
+            setAmount(''); // default value  after submit 
+            toast({
+                title: 'Congratulations',
+                description: "Your contribution has been added.",
+                status: 'success',
+                duration: 4000,
+                isClosable: true,
+              })
+        } catch (e) {
+            toast({
+                title: 'Error',
+                description: "An error accured.",
+                status: 'error',
+                duration: 4000,
+                isClosable: true,
+              })
+        }
 
-  return (
-    <>
-    <Heading mt="2rem">
-Contribute
-    </Heading>
+    }
 
-    <Flex  mt="1rem">
+    return (
+        <>
+            <Heading mt="2rem">
+                Contribute
+            </Heading>
 
-<Input placeholder="Your amount in ETH" 
-size="lg" 
-value={amount} 
-onChange={(e)=>setAmount(e.target.value )}
-/>
-<Button 
-colorScheme="purple"
-size='lg'
-onClick={()=>Contribute()}
->
-   Contribute 
-</Button>
-    </Flex>
-     </>
-  )
+            <Flex mt="1rem">
+
+                <Input placeholder="Your amount in ETH"
+                    size="lg"
+                    value={amount}
+                    onChange={(e) => setAmount(e.target.value)}
+                />
+                <Button
+                    colorScheme="purple"
+                    size='lg'
+                    onClick={() => contribute()}
+                >
+                    Contribute
+                </Button>
+            </Flex>
+        </>
+    )
 }
 
 export default Contribute
